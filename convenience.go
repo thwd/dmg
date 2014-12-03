ab1ef677fdc6d6c79def00a64f94336e3668a95b
Tom <sedevelopers01+thwd@gmail.com>
2014-12-03 23:42:23 +0100

    file organization

--- convenience.go
+++ convenience.go
@@ -14,24 +14,8 @@ func NewKleeneParser(p Parser) Parser {
 	})
 }
 
-func NewMutuallyRecursiveParsers(i int, f func([]Parser) []Parser) []Parser {
-
-	ps := make([]Parser, i, i)
-
-	for j := 0; j < i; j++ {
-		ps[j] = &RecursiveParser{}
-	}
-
-	qs := f(ps)
-
-	if len(qs) != len(ps) {
-		panic("len(qs) != len(ps)")
-	}
-
-	for j := 0; j < i; j++ {
-		ps[j].(*RecursiveParser).Parser = qs[j]
-	}
-
-	return ps
-
+func NewPlusParser(p Parser) Parser {
+	return NewSequenceParser(
+		p, NewKleeneParser(p),
+	)
 }
--- recursive.go
+++ recursive.go
@@ -15,3 +15,25 @@ func (p *RecursiveParser) Parse(bs Remnant) StateSet {
 		NewContinuedState(p.Parser, bs),
 	)
 }
+
+func NewMutuallyRecursiveParsers(i int, f func([]Parser) []Parser) []Parser {
+
+	ps := make([]Parser, i, i)
+
+	for j := 0; j < i; j++ {
+		ps[j] = &RecursiveParser{}
+	}
+
+	qs := f(ps)
+
+	if len(qs) != len(ps) {
+		panic("len(qs) != len(ps)")
+	}
+
+	for j := 0; j < i; j++ {
+		ps[j].(*RecursiveParser).Parser = qs[j]
+	}
+
+	return ps
+
+}
